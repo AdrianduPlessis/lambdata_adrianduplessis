921a9b03c3e2ee0888980e21bcc9052525851660
Adrian du Plessis <a3an.at@gmail.com>
2019-11-03 22:14:14 -0600

    complied with pep8

--- lambdata_adrianduplessis/df_utils.py
+++ lambdata_adrianduplessis/df_utils.py
@@ -4,16 +4,23 @@ import pandas as pd
 ONES = pd.Series(np.ones(20))
 ZEROS = pd.Series(np.zeros(20))
 
+
 def remove_observations_with_outliers(df, zscore=3):
-    #Outlier removal
+    '''
+    TODO
+    '''
+    # Outlier removal
     from scipy import stats
 
-    #Get subset within 'zscore' (default=3)  stdev
-    df_cleaned = df[(np.abs(stats.zscore(
-                        df.select_dtypes(
-                            include=np.number))) < 2).all(axis=1)]
+    # Get subset within 'zscore' (default=3)  stdev
+    df_cleaned = df[(
+        np.abs(stats.zscore(df.select_dtypes(include=np.number))) < 2).all(axis=1)]
 
     return df_cleaned
 
+
 def prove_that_this_even_works():
+    '''
+    Returns a string 'Proof.' as confirmation that the package works.
+    '''
     return 'Proof.'
