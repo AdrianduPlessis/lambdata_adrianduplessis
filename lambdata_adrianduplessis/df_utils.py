7479ac68849c958d0509134ae1f9685eca63ad9f
Adrian du Plessis <a3an.at@gmail.com>
2019-11-03 21:19:20 -0600

    create venv; add helper functions

--- lambdata_adrianduplessis/df_utils.py
+++ lambdata_adrianduplessis/df_utils.py
@@ -3,3 +3,17 @@ import pandas as pd
 
 ONES = pd.Series(np.ones(20))
 ZEROS = pd.Series(np.zeros(20))
+
+def remove_observations_with_outliers(df, zscore=3):
+    #Outlier removal
+    from scipy import stats
+
+    #Get subset within 'zscore' (default=3)  stdev
+    data_cleaned = data[(np.abs(stats.zscore(
+                        data.select_dtypes(
+                            include=np.number))) < 2).all(axis=1)]
+
+    return data_cleaned
+
+def prove_that_this_even_works():
+    return 'Proof.'
